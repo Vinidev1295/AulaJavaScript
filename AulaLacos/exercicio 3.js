e93934f490e2f8c89d84690aa55545a48869cdd1
Vinidev1295 <vinidias1295@gmail.com>
2024-11-13 00:53:47 -0300

    Add files via upload

---
+++ AulaLacos/exercicio 3.js
@@ -0,0 +1,11 @@
+const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}
+//Vai imprimir a quantidade de linhas e incrementar um asterico a cada linha ate atingir o número escrito, que no caso seria 4 linhas.
