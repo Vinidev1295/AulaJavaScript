840d4f9209b485cb892584befa231d671c520ffd
Vinidev1295 <vinidias1295@gmail.com>
2024-11-13 00:58:08 -0300

    Add files via upload

---
+++ AulaCondicionais/exercicio 5.js
@@ -0,0 +1,11 @@
+let verificar = prompt("Voce estuda em qual turno M/V/N?")
+let matutino = "M"
+let vespertino = "V"
+let noturno = "N"
+if(verificar==="M"){
+console.log("Bom Dia!")
+}else if(verificar==="V"){
+ console.log("Boa Tarde!")
+}else{
+ console.log("Boa Noite!")
+}
